1430c89ce08b596cdf024667a4caf0c8a92c7be0
Семенова Елизавета <thealeeeaaa23@gmail.com>
2023-12-06 21:25:32 +0300

    added template for index page

--- opinions_app.py
+++ opinions_app.py
@@ -1,15 +1,17 @@
 from datetime import datetime
-
 from random import randrange
 
-from flask import Flask
+# Добавлена функция render_template
+from flask import Flask, render_template  
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
 
 class Opinion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,13 +24,13 @@ class Opinion(db.Model):
 @app.route('/')
 def index_view():
     quantity = Opinion.query.count()
-    
     if not quantity:
-        return 'no.'
-    
+        return 'В базе данных мнений о фильмах нет.'
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
-    return opinion.text
+    # Вот он — новый возврат функции
+    return render_template('index.html', opinion=opinion)
+    
 
 if __name__ == '__main__':
     app.run()
